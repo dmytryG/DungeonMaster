b65d6251fd9e2295a412189ac6e724ed88478e4c
dmytryG <35425324+dmytryG@users.noreply.github.com>
2022-09-22 23:51:12 +0300

    first commit

---
+++ DungeonMaster/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Dungeon Master entry point");
+
+    }
+}
