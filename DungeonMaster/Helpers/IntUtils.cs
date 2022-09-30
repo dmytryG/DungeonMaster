02f971b70cf78aff29fad5fd7cad639524b54074
dmytryG <35425324+dmytryG@users.noreply.github.com>
2022-09-30 23:38:00 +0300

    Missing files

---
+++ DungeonMaster/Helpers/IntUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace DungeonMaster.Helpers;
+
+public class IntUtils
+{
+    public static bool IsIntInRange(int x, int a, int b)
+    {
+        if (a > b)
+            (a, b) = (b, a);
+        
+        var res = x >= a && x <= b;
+        return res;
+    }
+}
